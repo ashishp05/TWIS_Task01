e2c996b5c7ffb316830511c842d65308bf08664c
ashishp05 <121429162+ashishp05@users.noreply.github.com>
2024-04-11 12:44:37 +0530

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Stats = () => {
   return (
-    <div className='flex lg:flex-row md:flex-col md:gap-y-5 md:w-fit flex-col gap-y-5 mb-5  items-center justify-around  shadow-lg   rounded-md m-5 p-5 w-11/12 mx-auto '>
+    <div className='flex lg:flex-row md:flex-col md:gap-y-5  flex-col gap-y-5 mb-5  items-center justify-around  shadow-lg   rounded-md m-5 p-5 w-11/12 mx-auto  '>
         <div className='flex flex-col items-center text-center'>
             <div className='text-4xl font-inter font-semibold'>25+</div>
             <div className='text-[20px] text-richblack-300'>
